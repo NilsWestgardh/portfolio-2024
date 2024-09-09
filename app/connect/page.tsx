0154c0a935a7c715fa233466b53afceda95ddb41
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-09-09 21:42:31 +0200

    Add Cal.com embed

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -1,7 +1,63 @@
+"use client"
+
+import React, { useEffect } from "react";
+// Components
+import Cal, { getCalApi } from "@calcom/embed-react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card"
+
 export default function Connect() {
+
+  useEffect(()=>{
+	  (async function () {
+      const cal = await getCalApi();
+      cal("ui", {
+        "theme": "light",
+        "styles": {
+          "branding": {
+            "brandColor": "#000000"
+          }
+        },
+        "hideEventTypeDetails": false,
+        "layout": "month_view"
+      });
+	  })();
+	}, [])
+
   return (
-    <div>
-      <h1>Connect</h1>
-    </div>
+      <Card
+        className="
+          w-full 
+          max-w-4xl 
+          mx-auto 
+          shadow-none 
+          rounded-none 
+          border-0
+          mb-16
+        "
+      >
+        <CardHeader>
+          <CardTitle>Connect</CardTitle>
+          <CardDescription>Need a hand? Book a meeting at a time that suits you.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Cal 
+            calLink="nilswestgardh/30min"
+            style={{
+              width:"100%",
+              height:"100%",
+              overflow:"scroll"
+            }}
+            config={{
+              layout: 'month_view'
+            }}
+         />
+      </CardContent>
+    </Card>
   )
 }
