f3d978fa73f0b4bdaa1122be62dac09eaf873c56
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-09-10 13:13:27 +0200

    commit

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -81,13 +81,6 @@ export default function Navigation () {
                 </Button>
               </Link>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/resume">
-                <Button variant="ghost">
-                  Resume
-                </Button>
-              </Link>
-            </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
         <div className="hidden md:flex items-center space-x-2">
