b5e62ca053a70ce38ca65d69d433b23b6619ffc5
Nils Westgårdh <72079716+NilsWestgardh@users.noreply.github.com>
2024-12-25 12:36:18 +0100

    update api route

--- app/api/warmup/route.ts
+++ app/api/warmup/route.ts
@@ -2,6 +2,8 @@ import { getAllProjects } from "@/app/server/actions";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   try {
     const cookieStore = cookies();
